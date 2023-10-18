8ce50edd83bd2c61fe31f1d2826e64193aed65bd
Giulio Sidoretti <giuliosidoretti@gmail.com>
2023-10-18 08:24:48 +0000

    test script improved

--- test.sh
+++ test.sh
@@ -2,10 +2,16 @@
 
 for i in {1..10}
 do
-    for conn in {1..60}
+    for conn in {1..60} 65 70 75 80 85 90 95 100 110 120 130 140 150;
     do
         echo "Loop: " $i ", number of connections: " $conn
         req=$(($conn * 100))
-        ./bombardier -c $conn -n $req -o csv -x $conn.csv http://[fc12::2]
+        if [ -z $1 ]
+        then
+            pre=''
+        else
+            pre=$1-
+        fi
+        ./bombardier -c $conn -n $req -o csv -x $pre$conn.csv http://[fc12::2]
     done
 done
