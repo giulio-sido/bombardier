eb645d92dbe9ddc877d22c56b1d67c49e0ba596b
Giulio Sidoretti <giuliosidoretti@gmail.com>
2023-10-27 10:09:08 +0000

    test update

--- test.sh
+++ test.sh
@@ -8,19 +8,34 @@
 # 5: 300000/100000
 # 20: 1000000/100000
 
-for i in {1..30}
+conn=20
+
+for j in {1..20}
 do
-    # for conn in {1..60} `seq 62 2 100` `seq 105 5 200`
-    for conn in {1..60} `seq 62 2 80`
+    if [ $(($j%2)) -eq 0 ];
+    then
+        echo "without ebpf...";
+        ssh giu_sido@fc12::2 /users/giu_sido/eip-ml-ebpf/detach-ebpf.sh;
+        pre=$1-;
+    else
+        echo "with ebpf...";
+        ssh giu_sido@fc12::2 "cd /users/giu_sido/eip-ml-ebpf/ && ./restart-ebpf.sh";
+        pre=ebpf-$1-
+    fi
+    for i in {1..50}
     do
-        echo "Loop: " $i ", number of connections: " $conn
+        # for conn in {1..60} `seq 62 2 100` `seq 105 5 200`
+        # for conn in {1..60} `seq 62 2 80`
+        # do
+        echo "Loop: " $j " - " $i ", number of connections: " $conn
         req=$(($conn * 100))
-        if [ -z $1 ]
-        then
-            pre=''
-        else
-            pre=$1-
-        fi
+        # if [ -z $1 ]
+        # then
+        #     pre=''
+        # else
+        #     pre=$1-
+        # fi
         ./bombardier -c $conn -n $req -o csv -x $pre$conn.csv http://[fc12::2]
+        # done
     done
 done
